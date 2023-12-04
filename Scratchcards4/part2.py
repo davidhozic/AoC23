3db93d393692181bc8bed5046bf0e2115ca1d11d
David Hozic <davidhozic@gmail.com>
2023-12-04 11:12:08 +0100

    Day 4: Scratchcards

--- Scratchcards4/part2.py
+++ Scratchcards4/part2.py
@@ -3,26 +3,28 @@ with open("./input.txt") as file:
     cards: list[str] = file.readlines()
 
 
-
-i = 0
-while i < len(cards):
-    card = cards[i]
+# Preprocess original cards
+for i, card in enumerate(cards):
     card_numbers = card.split(":")
     card_index = int(card_numbers[0].split()[1])
     card_numbers = card_numbers[1]
     winning_n, scratched_n =  ''.join(card_numbers).split('|')
     winning_n = set(map(int, winning_n.strip().split()))
     scratched_n = set(map(int, scratched_n.strip().split()))
+    cards[i] = (card_index, winning_n, scratched_n)
+
+i = 0
+while i < len(cards):
+    card_index, winning_n, scratched_n = cards[i]
     n_winning = 0
     for win_number in winning_n:
         if win_number in scratched_n:
             n_winning += 1
-    
+
     # Since cards start count with 1, the card_index is actually the index of the original next card
     # inside the cards table
     next_cards = cards[card_index: card_index + n_winning]
     cards.extend(next_cards)
     i += 1
-
 
 print(len(cards))
